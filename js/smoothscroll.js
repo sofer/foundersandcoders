2e3808ee15501837673300d226c4fec483045db6
NataliaLKB <natalialeebaltazar@gmail.com>
2014-11-11 21:00:47 +0000

    added smooth school for contact as well

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,35 +1,15 @@
-// $(document).ready(function() {
-// 	$("a[href*=#]").on('click', function(e) {
+$(document).ready(function() {
+	$("a[href*=#]").on('click', function(e) {
 
-// 	   e.preventDefault();
-// 	   var hash = this.hash;
+	   e.preventDefault();
+	   var hash = this.hash;
 
-// 	   $('html, body').animate({
-// 	       scrollTop: $(this.hash).offset().top
-// 	     }, 1000, function(){
+	   $('html, body').animate({
+	       scrollTop: $(this.hash).offset().top
+	     }, 800, function(){
 
-// 	       window.location.hash = hash;
-// 	     });
+	       window.location.hash = hash;
+	     });
 
-// 	});
-// });
-
-function animate(elem,style,unit,from,to,time,prop) {
-    if( !elem) return;
-    var start = new Date().getTime(),
-        timer = setInterval(function() {
-            var step = Math.min(1,(new Date().getTime()-start)/time);
-            if (prop) {
-                elem[style] = (from+step*(to-from))+unit;
-            } else {
-                elem.style[style] = (from+step*(to-from))+unit;
-            }
-            if( step == 1) clearInterval(timer);
-        },25);
-    elem.style[style] = from+unit;
-}
-
-window.onload = function () {
-    var target = document.getElementById("aboutgo");
-    animate(document.body, "scrollTop", "", 0, target.offsetTop, 2000, true);
-};
+	});
+});
